a52b07089ec4ba5a10c4e2773f7ecb7a4e062cc6
Krishna Kariya <t-krkari@microsoft.com>
2020-08-17 21:36:09 +0530

    Created new REST and Django Project

--- Social_Influencer/urls.py
+++ Social_Influencer/urls.py
@@ -1,4 +1,4 @@
-"""Social_Influencer_backend URL Configuration
+"""Social_Influencer URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
--- Social_Influencer/wsgi.py
+++ Social_Influencer/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Social_Influencer_backend project.
+WSGI config for Social_Influencer project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Social_Influencer_backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Social_Influencer.settings')
 
 application = get_wsgi_application()
