9625f2ca142bda16c33526e035efcace2663a254
Krishnkariya <krishna17060@iiitd.ac.in>
2020-08-26 16:24:53 +0530

    Added Interests and Hobbies

--- social_media/admin.py
+++ social_media/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Social_Media
+from .models import Social_Media, Profile_Information
 # Register your models here.
 admin.site.register(Social_Media)
+admin.site.register(Profile_Information)
